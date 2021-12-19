08dd93d5e29ccbb646f6f873a822998b5c0922f7
Catur Saputro <50752647+lolimilkita@users.noreply.github.com>
2021-12-19 23:48:23 +0700

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Python-Data-Science-Project
  Project sederhana Data Science saya, menggunakan framework Streamlit
+
+# 1. Simple Stock Price Of Meta Platforms Inc (Facebook)
+<img src="https://github.com/lolimilkita/Python-Data-Science-Project/blob/main/img_readme/data_stock.png">
+<img src="https://github.com/lolimilkita/Python-Data-Science-Project/blob/main/img_readme/first.png">
---
+++ Simple Website Stock Price/app.py
@@ -0,0 +1,32 @@
+import matplotlib
+from matplotlib.backends.backend_agg import RendererAgg
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide")
+
+row1_1, row1_2 = st.columns((3,1))
+
+with row1_1:
+    st.title("Simple Stock Price Of Meta Platforms Inc (Facebook)")
+
+with row1_2:
+    st.write(
+    """
+    ##
+    Berikut adalah harga stock dan volume dari Meta Platforms!
+    Data dari tahun 2010 Desember 17 sampai tahun 2021 Desember 17
+    """)
+
+tickerSymbol = 'FB'
+
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='1d', start='2010-12-17', end='2021-12-17')
+
+st.subheader('Closing Price')
+st.line_chart(tickerDf.Close)
+
+st.subheader('Volume Price')
+st.line_chart(tickerDf.Volume)
